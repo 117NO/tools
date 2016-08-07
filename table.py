4406a3269c09b7ee2de88d962103c083f15d6107
root <root@debian>
2016-08-07 22:18:36 +0800

    update partial columns from different tables of different db

---
+++ table.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from sqlalchemy import Column, String, Integer,DateTime, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+class post(Base):
+    __tablename__   =   'wp_posts'
+    id              =   Column(Integer, primary_key=True)
+    post_title      =   Column(String)
+    post_content    =   Column(String)
+    post_date       =   Column(DateTime)
+    post_author    =   Column(String)
+
+
+class user(Base):
+    __tablename__   =   'wp_users'
+    id              =   Column(Integer, primary_key=True)
+    user_nicename   =   Column(String)
+    user_email      =   Column(String)
+    user_url        =   Column(String)
+    user_status     =   Column(Integer)
+
